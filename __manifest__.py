d967020330e55f74d5cb885d7b6669a5699a5a15
jchanme <39323414+jchanme@users.noreply.github.com>
2018-05-26 15:29:57 +0800

    Create __manifest__.py

---
+++ __manifest__.py
@@ -0,0 +1,14 @@
+# © initOS GmbH 2017
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Translate Company Information",
+    "version": "11.0.1.0.2",
+    "depends": ['base',
+        'hr',
+                ],
+    'author': 'Safyric Co., Ltd.',
+    "category": "Localization",
+    'license': 'AGPL-3',
+    'installable': True,
+}
