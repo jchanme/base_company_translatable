1d3d691e8e094576a259434cbf9989ce1bee6a18
jchanme <39323414+jchanme@users.noreply.github.com>
2020-03-13 20:24:05 +0800

    Update res_company.py

--- models/res_company.py
+++ models/res_company.py
@@ -1,11 +1,10 @@
 from odoo import fields, models
 
 
-class CompanyInfo(models.Model):
+class ResCompany(models.Model):
     _inherit = 'res.company'
 
     name = fields.Char(translate=True)
     city = fields.Char(translate=True)
     street = fields.Char(translate=True)
     street2 = fields.Char(translate=True)
-    report_header = fields.Text(translate=True)
